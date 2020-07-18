1ecd4d70c2ca6d35e3b90e1559e205ce3f860f92
Pedro Rodriguez <pedroroh22@gmail.com>
2020-07-17 21:18:37 -0500

    agregando cambios 17/07

--- ruleta.js
+++ ruleta.js
@@ -1,17 +1,13 @@
 puntos = 0;
 match = 0;
 
-
-
 let AddAgregar = docEleme("Agregar");
-
-
-let data;
-let botonPrev = document.querySelector("#Prev");
-let botonNext = document.querySelector("#Next");
+let arrPerson;
 let imgPersonaje = document.querySelector("#imgCollapse");
 
-const srcJson = "http://hdecstudio.com/api/Ruleta/service.php"
+
+
+const urlPhp = "http://hdecstudio.com/api/Ruleta/service.php"
 /*const srcJson = "http://127.0.0.1:5500/ruleta.json";*/
 let i = 0;
 
@@ -19,20 +15,22 @@ document.querySelector("#Prev").innerHTML = "<img id='btnPrev' src='img/back2.pn
 document.querySelector("#Next").innerHTML = "<img id='btnNext' src='img/next2.png' class='btn'>";
 document.querySelector("#tituloSelector").innerHTML = "<p style='text-aling:center;'>PERSONAJES ROBLOX</p>";
 
+let botonPrev = document.querySelector("#btnPrev");
+let botonNext = document.querySelector("#btnNext");
+
+
+
 function docEleme(id) {
     return document.getElementById(id);
 }
 
-//get
-/*$.ajax(urlPhp + "?name=" + docEleme("name").value +
-    "&number=" + docEleme("number").value + "&point=" +
-    docEleme("point").value + "&src=" + docEleme("src").value)
+//Obetner - GET
+$.ajax(urlPhp)
     .then(response => {
-        if (response.status == "0") {
-
-
+        if (response.statusCode == "0") {
+            data = response.data;
         }
-    })*/
+    })
 
 //post
 AddAgregar.addEventListener('click', () => {
@@ -43,8 +41,11 @@ AddAgregar.addEventListener('click', () => {
 
     const parametros = { name: NombreF, number: NumeroF, point: PonitF, url: UrlF };
 
+    //
 
-    fetch(srcJson, {
+
+    //Guardar - POST
+    fetch(urlPhp, {
         method: 'POST', // or 'PUT'
         body: JSON.stringify(parametros), // data can be `string` or {object}!
         headers: {
@@ -58,53 +59,60 @@ AddAgregar.addEventListener('click', () => {
 
 
 function consumirServicio() {
-    fetch(srcJson)
+    fetch(urlPhp)
         .then(respon => respon.json())
         .then(elemento => {
-            data = elemento;
+            console.log("1111111111");
 
-            var person = data.personaje[i];
-            var imagen = person.src;
-            var nombre = person.name;
-            var numero = person.number;
-            var punto = person.point;
+            if (elemento.statusCode == "0") {
+                arrPerson = elemento.data.personaje;
+                var person = arrPerson[i];
+                var imagen = person.src;
+                var nombre = person.name;
+                var numero = person.number;
+                var punto = person.point;
 
-            document.querySelector("#imgCollapse").innerHTML = "<img id='imagenPersonaje'  src='" +
-                imagen + "'>";
-            document.querySelector("#itemPersonaje").innerHTML = "<th>Numero</th><th>Puntos</th>"
-            document.querySelector("#datosPersonaje").innerHTML = "<tr><td>" + numero + "</td><td>" + punto + "</td></tr>"
-            document.querySelector("#cardHeader").innerHTML = "<p style='margin:0px;'>" + nombre + "</p>";
+                document.querySelector("#imgCollapse").innerHTML = "<img id='imagenPersonaje'  src='" +
+                    imagen + "'>";
+                document.querySelector("#itemPersonaje").innerHTML = "<th>Numero</th><th>Puntos</th>"
+                document.querySelector("#datosPersonaje").innerHTML = "<tr><td>" + numero + "</td><td>" + punto + "</td></tr>"
+                document.querySelector("#cardHeader").innerHTML = "<p style='margin:0px;'>" + nombre + "</p>";
+
+                debugger
+
+                botonPrev.addEventListener('click', (a, arrPerson) => {
+                    console.log(arrPerson.length);
+                    if (i <= 0) {
+                        i = arrPerson.length - 1;
+                    } else {
+                        i = i - 1;
+                    } //consumirServicio();
+                })
+
+                botonNext.addEventListener('click', (arrPerson) => {
+                    console.log("Boton Next");
+
+                    if (i >= arrPerson.length - 1) {
+                        i = 0;
+                    } else {
+                        i = i + 1;
+                    }
+                })
+
+
+            } else {
+                alert('Error Cavsa');
+            }
+
 
         })
 }
 
-function election() {
-    consumirServicio();
 
-    botonNext.addEventListener('click', () => {
-        if (i >= data.personaje.length - 1) {
-            i = 0;
-        } else {
-            i = i + 1;
-        } consumirServicio();
-    })
-
-    /* imgPersonaje.addEventListener('click', () => {
-          document.querySelector("#imgCollapse").click();
-      });*/
-
-    botonPrev.addEventListener('click', () => {
-        if (i <= 0) {
-            i = data.personaje.length - 1;
-        } else {
-            i = i - 1;
-        } consumirServicio();
-    })
-}
-
-//election();
+consumirServicio();
 
 
-$('.carousel').carousel({
+
+/*$('.carousel').carousel({
     interval: 0
-})
+})*/
